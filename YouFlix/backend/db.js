82f33a1632cc216648ffebdabc5d7967cb61f5a2
Andrew Gera <82167983+Andre-The-Greek@users.noreply.github.com>
2024-07-05 02:49:04 -0400

    Changed and added routes
    Changed the format of thumbnails to contain the id of its associated video instead of the other way around. Also added a route to get all thumbnail ids of a specific genre for the carousels, and a route to get the image and video id of a thumbnail for each item in the carousel. This needs to be tested though.

--- YouFlix/backend/db.js
+++ YouFlix/backend/db.js
@@ -2,13 +2,12 @@ import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const client = new MongoClient(process.env.MONGO_URI);
-
-async function connect() {
+async function connect(dbName) {
+    const client = new MongoClient(process.env.MONGO_URI);
     try {
         await client.connect();
         console.log('Connected to the database');
-        return client.db('videos');
+        return client.db(dbName);
     } catch (error) {
         console.error('Could not connect to the database', error);
         return null;
--- YouFlix/backend/upload.js
+++ YouFlix/backend/upload.js
@@ -3,42 +3,51 @@ import multer from 'multer';
 import connect from './db.js';
 import { GridFSBucket } from 'mongodb';
 
+//import fs from 'fs';
+
 const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-router.post('/upload', upload.fields([{ name: 'thumbnail', maxCount: 1 }, { name: 'video', maxCount: 1 }]), async (req, res) => {
+router.post('/upload/:genre', upload.fields([{ name: 'thumbnail', maxCount: 1 }, { name: 'video', maxCount: 1 }]), async (req, res) => {
     try {
         if (!req.files.video || !req.files.thumbnail) {
             return res.status(400).json({ msg: 'Video or thumbnail not uploaded' });
         }
 
-        const client = await connect();
-        const bucket = new GridFSBucket(client, { bucketName: 'videos' });
+        const videoDb = await connect('videos');
+        const bucket = new GridFSBucket(videoDb, { bucketName: 'videos' });
 
-        const thumbnailStream = bucket.openUploadStream(req.files.thumbnail[0].originalname);
-        thumbnailStream.write(req.files.thumbnail[0].buffer);
-        thumbnailStream.end();
+        const thumbDb = await connect('thumbnails');
 
-        const thumbnailUploadPromise = new Promise((resolve, reject) => {
-            thumbnailStream.on('finish', resolve);
-            thumbnailStream.on('error', reject);
-        });
-
-        const thumbnailFileInfo = await thumbnailUploadPromise;
-
-        const videoStream = bucket.openUploadStream(req.files.video[0].originalname, {
-            metadata: { thumbnailId: thumbnailFileInfo._id.toString() },
-        });
+        const videoStream = bucket.openUploadStream(req.files.video[0].originalname);
         videoStream.write(req.files.video[0].buffer);
         videoStream.end();
 
-        videoStream.on('finish', () => {
+        const videoUploadPromise = new Promise((resolve, reject) => {
+            videoStream.on('finish', resolve);
+            videoStream.on('error', reject);
+        })
+
+        const videoFileInfo = await videoUploadPromise;
+
+        const thumbnailStream = bucket.openUploadStream(req.files.thumbnail[0].originalname, {
+            metadata: { videoId: videoFileInfo._id },
+        });
+        thumbnailStream.write(req.files.thumbnail[0].buffer);
+        thumbnailStream.end();
+
+        thumbnailStream.on('finish', () => {
+            thumbDb.collection('ids').updateOne({ genre: req.params.genre }, {
+                $push: { thumbnails: thumbnailFileInfo._id },
+            }, {
+                upsert: true,
+            });
             res.status(201).json({
                 video: req.files.video[0].originalname,
                 thumbnail: req.files.thumbnail[0].originalname,
-                videoId: videoStream.id,
-                thumbnailId: thumbnailFileInfo._id
+                videoId: videoFileInfo._id.toString(),
+                thumbnailId: thumbnailFileInfo._id.toString()
             });
         });
     } catch (error) {
@@ -48,12 +57,13 @@ router.post('/upload', upload.fields([{ name: 'thumbnail', maxCount: 1 }, { name
 });
 
 router.get('/getVideo/:id', async (req, res) => {
-    const db = await connect();
+    const db = await connect('videos');
     if (db == null) {
         res.status(500).json({ msg: 'Could not connect to the database' });
         return;
     }
-    const bucket = new GridFSBucket(db, { bucketName: 'videos' });
+    const test = await db.collection('files').find({}).toArray();
+    /* const bucket = new GridFSBucket(db, { bucketName: 'videos' });
     const cursor = bucket.find({ _id: req.params.id });
     const video = await cursor.toArray();
     if (video.length === 0) {
@@ -84,9 +94,9 @@ router.get('/getVideo/:id', async (req, res) => {
         start:start,
         end:end
     });
-    downloadStream.pipe(res);
+    downloadStream.pipe(res); */
     //for testing the route
-    /* const path = './videos/vid1.mp4';
+    const path = './videos/vid1.mp4';
     const stat = fs.statSync(path);
     const fileSize = stat.size;
     const range = req.headers.range;
@@ -113,8 +123,35 @@ router.get('/getVideo/:id', async (req, res) => {
         };
         res.writeHead(200, head);
         fs.createReadStream(path).pipe(res);
-    } */
+    }
 });
 
+router.get('/getThumbnails/:genre', async (req, res) => {
+    const db = await connect('thumbnails');
+    if (db == null) {
+        res.status(500).json({ msg: 'Could not connect to the database' });
+        return;
+    }
+    const thumbnails = await db.collection('ids').find({ genre: req.params.genre }).toArray();
+    res.status(200).json(thumbnails.thumbnails);
+});
+
+router.get('/getThumbnail/:id', async (req, res) => {
+    const db = await connect('videos');
+    if (db == null) {
+        res.status(500).json({ msg: 'Could not connect to the database' });
+        return;
+    }
+    const bucket = new GridFSBucket(db, { bucketName: 'videos' });
+    const cursor = bucket.find({ _id: req.params.id });
+    const thumbnail = await cursor.toArray();
+    if (thumbnail.length === 0) {
+        res.status(404).json({ msg: 'Thumbnail not found' });
+        return;
+    }
+    res.status(200).json({videoId: thumbnail[0].metadata.videoId.toString()});
+    const downloadStream = bucket.openDownloadStream(thumbnail[0]._id);
+    downloadStream.pipe(res);
+});
 
 export default router;
